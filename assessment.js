e86144fd40c29148042a6ff25904a409f9f2d469
gessn <39127320+gessn@users.noreply.github.com>
2018-05-09 23:15:39 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -6,7 +6,7 @@
     const tweetDivided = document.getElementById('tweet-area');
 
     /**
-    * 指定した要素の子どもを全て削除する
+    * 指定した要素の子どもを全て除去する
     * @param {HTMLElement} element HTMLの要素
     */
     function removeAllChildren(element) {
@@ -15,20 +15,11 @@
         }
     }
 
-    userNameInput.onkeydown = (event) => {
-        if (event.keyCode === 13) {
-            // TODO ボタンのonclick() 処理を呼び出す
-            assessmentButton.onclick();
-        }
-    };
-
     assessmentButton.onclick = () => {
         const userName = userNameInput.value;
         if (userName.length === 0) { // 名前が空の時は処理を終了する
             return;
         }
-
-    
 
         // 診断結果表示エリアの作成
         removeAllChildren(resultDivided);
@@ -41,20 +32,23 @@
         paragraph.innerText = result;
         resultDivided.appendChild(paragraph);
 
-        // TODO ツイートエリアの作成
+        // ツイートエリアの作成
         removeAllChildren(tweetDivided);
         const anchor = document.createElement('a');
-        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
-            + encodeURIComponent('あなたのいいところ')
-            + '&ref_src=twsrc%5Etfw';
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
+        + encodeURIComponent(result);
         anchor.setAttribute('href', hrefValue);
         anchor.className = 'twitter-hashtag-button';
-        anchor.setAttribute('data-text', result);
-        anchor.setAttribute('data-lang', 'ja');
-        anchor.setAttribute('data-show-sount', 'false');
-        anchor.innerText = 'あなたのいいところ　をツイートする';
+        anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
         tweetDivided.appendChild(anchor);
+
         twttr.widgets.load();
+    };
+
+    userNameInput.onkeydown = (event) => {
+        if (event.keyCode === 13) {
+            assessmentButton.onclick();
+        }
     };
 
     const answers = [
@@ -74,7 +68,7 @@
         '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
         '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
         '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
-        '{userName}のいいところは優しさです。{userName}の優しい雰囲気や立ち振舞に多くの人が癒やされています。'
+  　　　　　　　　  '{userName}のいいところは優しさです。{userName}の優しい雰囲気や立ち振舞に多くの人が癒やされています。'
     ];
 
     /**
